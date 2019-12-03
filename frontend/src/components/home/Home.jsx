161151d7d10e68b348946243a05ac174131c794e
João Victor <victorpow@live.com>
2019-12-03 18:51:52 -0300

    first commit

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+subtitle="Consumindo recursos de uma API.">
+<div className='display-4'>Bem-vindo! 
+</div>
+<hr />
+<p className="mb-0">Projeto Integrado - Consumindo recursos de uma API</p>
+    </Main>
---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-heard text-danger"></i> por
+            <strong><span className="text-danger">João</span></strong>
+        </span>
+    </footer>
